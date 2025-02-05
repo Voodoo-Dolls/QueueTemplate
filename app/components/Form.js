5655475b723e526f1be18e2c5ed983ea968f89a0
Michael Dam <michaeldam.dev@gmail.com>
2025-02-04 18:11:32 -0700

    Updated Password

--- app/components/Form.js
+++ app/components/Form.js
@@ -1,6 +1,9 @@
 "use client";
 import { useState } from "react";
 import MarkdownComponent from "./Markdown/Markdown";
+import { FaImage } from "react-icons/fa6";
+import { IoDocumentTextOutline } from "react-icons/io5";
+
 import { FaDice } from "react-icons/fa";
 
 import Link from "next/link";
@@ -19,7 +22,7 @@ const Form = () => {
     mm: 48,
     zedType: "Vanilla",
     startTime: "ASAP",
-    password: "YorQ&^B{1u!80+dC",
+    password: "7!@bzVot+H1X9M0f",
   });
 
   const markdown = `
@@ -265,23 +268,47 @@ open 74.91.113.4:6999?password=${settings.password}
                 onChange={(event) => handleFormChange(event)}
               />
             </div>
+            <div>
+              <div className="mb-6">
+                <label
+                  htmlFor="password"
+                  className="mb-2 text-sm font-medium text-gray-900 dark:text-white flex gap-2"
+                >
+                  Whitelist Override
+                </label>
+                <input
+                  type="text"
+                  name="password"
+                  id="password"
+                  value={settings.password}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  onChange={(event) => handleFormChange(event)}
+                />
+              </div>
+            </div>
           </div>
           {/* Copy to Clip Board */}
-          <div>
+          <div className="flex gap-4">
             <button
               type="button"
-              className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+              className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800 flex gap-2 items-center"
               onClick={copyImgToClipboard}
             >
+              <span className="text-xl">
+                <FaImage />
+              </span>
               Copy Thumbnail
             </button>
             <button
               type="button"
-              className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+              className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800 flex items-center gap-2"
               onClick={() => {
                 navigator.clipboard.writeText(markdown);
               }}
             >
+              <span className="text-xl">
+                <IoDocumentTextOutline />
+              </span>
               Copy Template
             </button>
           </div>
